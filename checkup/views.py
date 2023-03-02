42eab2886211b36f76f437b4588c180ceb99175a
wamah <pythondeveloper21@gmail.com>
2023-03-02 22:02:36 +0000

    Just the beginning

--- checkup/views.py
+++ checkup/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hello(request):
-    return HttpResponse("<h2>Hello Here we come</h2>")
+    return HttpResponse("<h2>Hello Here we come??</h2>")
